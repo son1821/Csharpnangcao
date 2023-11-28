1200a577ceb2cd6633fcbd719829e90ad7ab9767
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-28 23:17:58 +0700

    Generic

---
+++ Generic1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic1
+{
+    class Class1<T>
+    {
+        public T Value { get; set; }
+        
+        public Class1(T value)
+        {
+            Value = value;
+        }
+        public void ToString()
+        {
+            Console.WriteLine("Value = "+Value);
+        }
+       
+    }
+}
---
+++ Generic1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Generic1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Sum<int>(ref a,ref b);
+            string c = "Son";
+            string d = "Quynh";
+            Sum<string>(ref c,ref d);
+            Class1<int> class1 = new Class1<int>(5);
+            class1.ToString();
+
+        }
+        public static void Sum<T>(ref T a, ref T b)
+        {
+            Console.WriteLine(a); Console.WriteLine(b);
+        }
+    }
+}
