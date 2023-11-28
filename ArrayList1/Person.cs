35ccbdcff09d99ef6ada477a87b6d883dc2e8014
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-28 09:26:22 +0700

    Add project files.

---
+++ ArrayList1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void ToString()
+        {
+            Console.Write("Name: " + Name+" ");
+            Console.WriteLine("Age: " + Age);
+        }
+    }
+}
---
+++ ArrayList1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace ArrayList1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrPersons = new ArrayList();
+            arrPersons.Add(new Person("Son", 18));
+            arrPersons.Add(new Person("Quang", 25));
+            arrPersons.Add(new Person("Hai", 15));
+            Console.WriteLine("Danh sach ban dau");
+            foreach (Person person in arrPersons)
+            {
+                person.ToString();
+            }
+            arrPersons.Sort(new SortPersons());
+            Console.WriteLine();
+            Console.WriteLine("Danh sach sau khi sap xep tuoi tang dan");
+            foreach (Person person in arrPersons)
+            {
+                person.ToString();
+            }
+        }
+    }
+    public class SortPersons : IComparer
+    {
+     public int Compare(object x, object y)
+        {
+            Person person1 = x as Person;
+            Person person2 = y as Person;
+            if(person1 == null && person2 == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                if(person1.Age < person2.Age)
+                {
+                    return -1;
+                }else if(person1.Age == person2.Age)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
