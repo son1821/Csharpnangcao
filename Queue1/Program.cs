ab5f5dbb6e2d78efe14f0212e5dde7d196f988a2
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-28 16:47:46 +0700

    Queue

---
+++ Queue1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace Queue1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("Son");
+            queue.Enqueue("Quynh");
+            queue.Enqueue("Chau");
+            Console.WriteLine("Name: "+queue.Peek());
+            Console.WriteLine("So phan tu hien co la: " + queue.Count);
+            Console.WriteLine();
+            Console.WriteLine("Phan tu dau tien la: " + queue.Dequeue());
+            Console.WriteLine("So phan tu hien co la: " + queue.Count);
+            Console.ReadLine();
+        }
+    }
+}
