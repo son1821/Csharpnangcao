1fc84264a443d25b53aa4a1c2b8b07e1b527fc16
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-29 23:02:21 +0700

    Event_Delegate

---
+++ Event_Delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Event_Delegate
+{
+    delegate void UpdateNameHandler(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HocSinh hs = new HocSinh();
+            hs.NameChanged += Hs_NameChanged;
+            hs.Name = "Son";
+            Console.WriteLine("Ten tu class Program: "+hs.Name);
+            hs.Name = "Quynh";
+            Console.WriteLine("Ten tu class Program: " + hs.Name);
+        }
+
+        private static void Hs_NameChanged(string name)
+        {
+            Console.WriteLine("Ten moi: "+name);
+        }
+    }
+    class HocSinh
+    {
+        public event UpdateNameHandler NameChanged;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                name = value;
+                NameChanged(Name);
+            }
+        }
+        
+    }
+}
