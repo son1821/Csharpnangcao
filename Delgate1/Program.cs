4cbb66c40b4e6a7f436dd4a1b5bc3261351c6ef9
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-29 22:15:21 +0700

    Delegate

---
+++ Delgate1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Delegate1
+{
+    class Program
+    {
+        delegate int MyDelegate(string x);
+        public static void Main(string[] args)
+        {
+            MyDelegate showString = new MyDelegate(ShowString);
+            NhapTen(showString);
+            //MyDelegate myDelegate1 = new MyDelegate(ShowString);
+            //MyDelegate muticast = myDelegate + myDelegate1;
+            //string number = "15";
+            //myDelegate(number);
+            //myDelegate1(number);
+            //Console.WriteLine("Hien thi sau khi dung muticast: ");
+            //muticast(number);
+           
+        }
+        static void NhapTen(MyDelegate myDelegate)
+        {
+
+            Console.WriteLine("Moi nhap ten");
+            string ten = Console.ReadLine();
+            myDelegate(ten);
+
+        }
+        //public static int ConvertToInt(string x)
+        //{
+        //    int value = Convert.ToInt32(x);
+        //    Console.WriteLine("Da ep kieu thanh cong ");
+            
+        //    return value ;
+        //}
+        public static int ShowString(string stringValue) 
+        {
+             Console.WriteLine(stringValue);
+            return 0 ;
+        }
+       
+        }
+    }
+
