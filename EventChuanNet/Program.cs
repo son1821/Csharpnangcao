7105004893845d44674179cde88c9d680ac8ce90
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-30 00:44:03 +0700

    EventChuanNet

---
+++ EventChuanNet/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace EventChuanNet
+{
+    delegate void UpdateNameHandler(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HocSinh hs = new HocSinh();
+            hs.NameChanged += Hs_NameChanged;
+            hs.Name = "Son";
+            hs.Name = "Quynh";
+        }
+
+        private static void Hs_NameChanged(object sender, NameChangedEventArgs e)
+        {
+            Console.WriteLine("Da dươc thay doi: "+e.Name);
+        }
+    }
+    class HocSinh
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set 
+            { name = value;
+              OnNameChanged(value);
+            }
+        }
+        private event EventHandler<NameChangedEventArgs> nameChanged;
+        public event EventHandler<NameChangedEventArgs> NameChanged
+        {
+            add
+            {
+              nameChanged += value;
+            }
+            remove
+            {
+                nameChanged -= value;
+            }
+        }
+        public void OnNameChanged(string name)
+        {
+            nameChanged(this, new NameChangedEventArgs(name));
+        }
+        
+    }
+    public class NameChangedEventArgs: EventArgs
+    {
+        public string Name { get; set; }
+        public NameChangedEventArgs(string name)
+        {
+            Name = name;
+        }   
+    }
+}
+
+
