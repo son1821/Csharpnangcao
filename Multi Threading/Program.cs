6adff76606f16aa84e2dad448299710714f00b35
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-30 23:55:26 +0700

    MultiThreading

---
+++ Multi Threading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+namespace Multi_Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Thread t2 = new Thread(() => 
+            {
+                Demo("New Thread: ");
+            });
+            t2.IsBackground = true;
+            t2.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main Thread: "+ i);
+                Thread.Sleep(1000);
+            }
+
+        }
+       
+        public static void Demo(string a) { 
+        for (int i = 0; i < 10; i++) {
+                Console.WriteLine(a+" "+i);
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            }
+        }
+    }
+}
